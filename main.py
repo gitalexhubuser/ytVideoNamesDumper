a06074dca5a633927c414d123249442933e0487d
gitalexhubuser <alexdubovyck@yandex.ru>
2023-10-28 01:49:38 +0300

    fireFox - только 3 видео(

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
-import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
 
-# url = 'https://www.youtube.com/@alex.dubovyck.videos/videos'
-# url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCjDdSdLJbbV0UBtzKpClmig' # me
-url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCVws9TRZN49QxiIk4Q3VOgg' # atom
+# Путь к исполняемому файлу драйвера Selenium WebDriver
+# webdriver_path = r'E:\PythonProjects\ytVideoNamesDumper\Assets\webdriver.exe' ## https://chromedriver.storage.googleapis.com/index.html?path=114.0.5735.90/
+webdriver_path = r'E:\PythonProjects\ytVideoNamesDumper\Assets\geckodriver.exe' ## https://github.com/mozilla/geckodriver/releases
 
-def downloadSiteToFile(url):
-    response = requests.get(url)
+# Опции драйвера
+# options = Options()
+# options.add_argument('--headless')  # Запуск в безголовом режиме (без отображения браузера)
 
-    if response.status_code == 200:
-        print("Ответ 200!")
+# Создание экземпляра драйвера
+driver = webdriver.Firefox(service=Service(executable_path=webdriver_path))
 
-        source = response.text
-        # print("source:", source)
+# Загрузка страницы
+# driver.get('https://www.youtube.com/@alex.dubovyck.videos/videos')
+driver.get('https://www.youtube.com/@LuaNaZakaz/videos')
 
-        soup = BeautifulSoup(source, "xml")  # html.parser lxml
-        
-        media_title_tags = soup.find_all('media:title')
-        for media_title_tag in media_title_tags:
-            text_after_title = media_title_tag.get_text(strip=True)
-            print(text_after_title)
+# Ждем, пока все заголовки полностью загрузятся (вы можете настроить это время в зависимости от сайта)
+driver.implicitly_wait(60)
 
+# Получаем все заголовки с помощью XPath
+# media_title_elements = driver.find_elements(By.ID, 'video-title')
+# media_title_elements = driver.find_elements(By.CLASS_NAME, 'style-scope ytd-rich-grid-media')
+media_title_elements = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
 
+# Выводим текст каждого заголовка
+for element in media_title_elements:
+    text_after_title = element.text.strip()
+    print(text_after_title)
 
-if __name__ == "__main__":
-    downloadSiteToFile(url)
-    # findAllVideosInFile("./Assets/index.html")
-    # findAllVideosInFile(r"E:\PythonProjects\ytVideoNamesDumper\Assets\index.html")
+# Закрытие драйвера
+# driver.quit()
