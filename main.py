e94d4df27ab62fd0040fc4dc1be15e7041af66c1
gitalexhubuser <alexdubovyck@yandex.ru>
2023-10-28 00:09:40 +0300

    ничего не принтует(

--- main.py
+++ main.py
@@ -13,27 +13,33 @@ def downloadSiteToFile(url):
         print("Ответ 200!")
 
         source = response.text
-        print("source: ", source)
+        print("source:", source)
 
         # Запись содержимого в файл
         with open("./Assets/index.html", "w", encoding="utf-8") as file:
             file.write(source)
 
-        print("Сайт успешно сохранен в файле test.html")
+        print("Сайт успешно сохранен в файле index.html")
 
 
 def findAllVideosInFile(filePath):
     with open(filePath, encoding="utf8", errors='ignore') as file:
         source = file.read()
-        print(f"source: {source}")
+        # print(f"source: {source}")
 
-        # sirus_site = BeautifulSoup(source, "lxml")
+        if source is not None:
+            soup = BeautifulSoup(source, "lxml")
+            # print(f"soup: {soup}")
 
-        # if source is not None:
-        #     sirus_site = BeautifulSoup(source, "lxml")
-            ###########################################
+            # Имя
+            videos = soup.find_all(class_="style-scope ytd-rich-grid-media")  # Замените "your-class-name" на класс, который вы ищете
+
+            for video in videos:
+                print(video)
+                # print(video.text)
+
 
 
 if __name__ == "__main__":
-    downloadSiteToFile(url)
-    downloadSiteToFile("./Assets/index.html")
+    # downloadSiteToFile(url)
+    findAllVideosInFile("./Assets/index.html")
