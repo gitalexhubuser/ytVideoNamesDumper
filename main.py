945abc00d0b7456989c154388f1e26ec5ed70440
gitalexhubuser <alexdubovyck@yandex.ru>
2023-10-28 19:04:16 +0300

    +wylsacom

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ driver = webdriver.Firefox(service=Service(executable_path=webdriver_path)) # С
 # driver.get('https://www.youtube.com/@alex.dubovyck.videos/videos')
 # driver.get('https://www.youtube.com/@LuaNaZakaz/videos')
 # driver.get('https://www.youtube.com/@DarhangeR_Official/videos')
-driver.get('https://www.youtube.com/@AccLeiTo/videos')
+# driver.get('https://www.youtube.com/@AccLeiTo/videos')
+# driver.get('https://www.youtube.com/@OffDuchess/videos')
+driver.get('https://www.youtube.com/@Wylsacom/videos')
 
 channel_name = driver.current_url.split('.youtube.com/')[1].replace('/videos', '') # Получаем название канала из URL
 # print("channel_name", channel_name) # @AccLeiTo/videos
@@ -35,7 +37,7 @@ time.sleep(2)
 
 # Пролистываем страницу вниз
 start_time = time.time()
-duration = 4  # Длительность в секундах
+duration = 232  # Длительность в секундах
 while time.time() - start_time < duration:
     element = driver.find_element(By.TAG_NAME, 'body') # Находим любой элемент на странице и фокусируемся на нем
     element.send_keys(Keys.END) # Пролистываем страницу вниз
@@ -43,6 +45,7 @@ while time.time() - start_time < duration:
     
 
 media_title_elements = driver.find_elements(By.XPATH, '//*[@id="video-title"]') # (By.CSS_SELECTOR, '#video-title')
+# media_title_elements = driver.find_elements(By.CSS_SELECTOR, '#video-title') # (By.CSS_SELECTOR, '#video-title')
 # print(len(media_title_elements))
 strMediaTitleElements = str(len(media_title_elements)) # Кол-во видосов строкой
 
@@ -51,7 +54,12 @@ with open(file_name, 'w', encoding='utf-8') as file:
     # Выводим текст каждого заголовка
     for element in media_title_elements:
         text_after_title = element.text.strip()
-        print(text_after_title)
+        # print(text_after_title)
         file.write(text_after_title + '\n')
     print(strMediaTitleElements)
     file.write(strMediaTitleElements + '\n')
+
+time.sleep(5)
+
+# Закрытие драйвера
+driver.quit()
