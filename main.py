cd0e202397745341fe13bd5b80351662cd0d6c16
gitalexhubuser <alexdubovyck@yandex.ru>
2023-10-28 20:00:47 +0300

    Только  если пользователь ввёл

--- main.py
+++ main.py
@@ -5,62 +5,66 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 webdriver_path = r'E:\PythonProjects\ytVideoNamesDumper\Assets\geckodriver.exe' ## https://github.com/mozilla/geckodriver/releases
-driver = webdriver.Firefox(service=Service(executable_path=webdriver_path)) # Создание экземпляра драйвера
 
-# Загрузка страницы
-# driver.get('https://www.youtube.com/@alex.dubovyck.videos/videos')
-driver.get('https://www.youtube.com/@LuaNaZakaz/videos')
-# driver.get('https://www.youtube.com/@DarhangeR_Official/videos')
-# driver.get('https://www.youtube.com/@AccLeiTo/videos')
-# driver.get('https://www.youtube.com/@OffDuchess/videos')
-# driver.get('https://www.youtube.com/@Wylsacom/videos')
-# driver.get('https://www.youtube.com/@zvezdo4kina/videos')
+while True:
+    print('\r\nEnter channel name:')
+    print('Example: https://www.youtube.com/@alex.dubovyck.videos/videos')
+    m = input()
 
-channel_name = driver.current_url.split('.youtube.com/')[1].replace('/videos', '') # Получаем название канала из URL
-# print("channel_name", channel_name) # @AccLeiTo/videos
+    if m:
+        # Загрузка страницы
+        driver = webdriver.Firefox(service=Service(executable_path=webdriver_path)) # Создание экземпляра драйвера
+        driver.get(m)
+        # driver.get('https://www.youtube.com/@alex.dubovyck.videos/videos')
 
-# Создаем папку для сохранения результатов, если она не существует
-if not os.path.exists('results'):
-    os.mkdir('results')
-    print("Создал папку - results")
+        channel_name = driver.current_url.split('.youtube.com/')[1].replace('/videos', '') # Получаем название канала из URL
+        # print("channel_name", channel_name) # @AccLeiTo/videos
 
-file_name = f'results/{channel_name}.txt' # Создаем имя файла на основе названия канала
+        # Создаем папку для сохранения результатов, если она не существует
+        if not os.path.exists('results'):
+            os.mkdir('results')
+            print("Создал папку - results")
 
-
-
-
-# Ждем, пока все заголовки полностью загрузятся (вы можете настроить это время в зависимости от сайта)
-driver.implicitly_wait(2)
-time.sleep(2)
+        file_name = f'results/{channel_name}.txt' # Создаем имя файла на основе названия канала
 
 
 
 
-# Пролистываем страницу вниз
-start_time = time.time()
-duration = 12  # Длительность в секундах
-while time.time() - start_time < duration:
-    element = driver.find_element(By.TAG_NAME, 'body') # Находим любой элемент на странице и фокусируемся на нем
-    element.send_keys(Keys.END) # Пролистываем страницу вниз
-    time.sleep(0.5)
-    
 
-media_title_elements = driver.find_elements(By.XPATH, '//*[@id="video-title"]') # (By.CSS_SELECTOR, '#video-title')
-# media_title_elements = driver.find_elements(By.CSS_SELECTOR, '#video-title') # (By.CSS_SELECTOR, '#video-title')
-# print(len(media_title_elements))
-strMediaTitleElements = str(len(media_title_elements)) # Кол-во видосов строкой
+        # Ждем, пока все заголовки полностью загрузятся (вы можете настроить это время в зависимости от сайта)
+        driver.implicitly_wait(3)
+        time.sleep(3)
 
-# Открываем файл в режиме записи
-with open(file_name, 'w', encoding='utf-8') as file:
-    # Выводим текст каждого заголовка
-    for element in media_title_elements:
-        text_after_title = element.text.strip()
-        # print(text_after_title)
-        file.write(text_after_title + '\n')
-    print(strMediaTitleElements)
-    file.write(strMediaTitleElements + '\n')
 
-time.sleep(5)
 
-# Закрытие драйвера
-driver.quit()
+
+        # Пролистываем страницу вниз
+        start_time = time.time()
+        duration = 20  # Длительность в секундах
+        while time.time() - start_time < duration:
+            element = driver.find_element(By.TAG_NAME, 'body') # Находим любой элемент на странице и фокусируемся на нем
+            element.send_keys(Keys.END) # Пролистываем страницу вниз
+            time.sleep(0.1)
+            element.send_keys(Keys.END) # Пролистываем страницу вниз
+            time.sleep(1)
+            
+
+        media_title_elements = driver.find_elements(By.XPATH, '//*[@id="video-title"]') # (By.CSS_SELECTOR, '#video-title')
+        # media_title_elements = driver.find_elements(By.CSS_SELECTOR, '#video-title') # (By.CSS_SELECTOR, '#video-title')
+        # print(len(media_title_elements))
+        strMediaTitleElements = str(len(media_title_elements)) # Кол-во видосов строкой
+
+        # Открываем файл в режиме записи
+        with open(file_name, 'w', encoding='utf-8') as file:
+            # Выводим текст каждого заголовка
+            for element in media_title_elements:
+                text_after_title = element.text.strip()
+                # print(text_after_title)
+                file.write(text_after_title + '\n')
+            print(strMediaTitleElements)
+            file.write(strMediaTitleElements + '\n')
+
+        time.sleep(5)
+
+        # Закрытие драйвера
+        driver.quit()
---
+++ run.cmd
@@ -0,0 +1,2 @@
+cmd /k "cd /d E:\PythonProjects\ytVideoNamesDumper\venv\Scripts & activate & cd /d E:\PythonProjects\ytVideoNamesDumper & python main.py"
+@REM pause
